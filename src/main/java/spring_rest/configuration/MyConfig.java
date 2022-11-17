c92fc444d39d2cc78b510e70580a4e5b29a37c35
Vladimir <sokolov_elista@mail.ru>
2022-11-17 21:56:45 +0300

    Final commit new project - Spring MVC REST API

--- src/main/java/spring_rest/configuration/MyConfig.java
+++ src/main/java/spring_rest/configuration/MyConfig.java
@@ -25,7 +25,7 @@ public class MyConfig {
         try {
             dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
             dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC");
-//            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+//            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC"); //Неверное написание
             dataSource.setUser("bestuser");
             dataSource.setPassword("bestuser");
         } catch (PropertyVetoException e) {
